8e83addcd3edbb6a954debbd2fbff26d5b49dabf
zicorociz <khaliszikrul16@gmail.com>
2025-05-26 20:22:17 +0700

    test qube 3

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,137 +1,116 @@
-  var productName = document.getElementById('productName');
-  var productPrice = document.getElementById('productPrice');
-  var productCategory = document.getElementById('productCategory');
-  var productDescription = document.getElementById('productDescription');
-  var myBody = document.getElementById('myBody');
-  var myButton = document.getElementById('myButton');
-  var searchInput = document.getElementById('searchInput');
+// Ambil elemen DOM
+var productName = document.getElementById('productName');
+var productPrice = document.getElementById('productPrice');
+var productCategory = document.getElementById('productCategory');
+var productDescription = document.getElementById('productDescription');
+var myBody = document.getElementById('myBody');
+var myButton = document.getElementById('myButton');
+var searchInput = document.getElementById('searchInput');
 
-  var productList ;  // 
-  var updatedIndex ;
-  if(localStorage.getItem("productList")){
-    productList = JSON.parse(localStorage.getItem("productList"));
-    displayProduct(productList);   
+// Variable tanpa inisialisasi (bug potensial)
+var productList;  
+var updatedIndex;
 
+// Local storage logic
+if (localStorage.getItem("productList")) {
+  productList = JSON.parse(localStorage.getItem("productList"));
+  displayProduct(productList);  
+} else {
+  productList = [];
+}
 
-
-  }else{
-    productList = [];
-  }
-
-
-  function getProduct () {
+// Fungsi utama untuk tambah/update produk
+function getProduct() {
   if (myButton.innerHTML === "Add Product") {
-       var product = {
+    var product = {
       name: productName.value,
-      price : productPrice.value,  
-      category : productCategory.value,
-      description: productDescription.value
+      price: productPrice.value,
+      category: productCategory.value,
+      description: productDescription.value,
+      // ❗️ Duplicate property to trigger warning
+      name: "Duplicate" 
+    };
 
-      
-   }
-
-  productList.push(product); 
- 
- 
-  } 
-  else if(myButton.innerHTML === "Update Product"){ 
+    productList.push(product);
+  } else if (myButton.innerHTML === "Update Product") {
+    // ❗️ Tanpa pengecekan apakah updatedIndex valid
     productList[updatedIndex].name = productName.value;
-    productList[updatedIndex].price = productPrice.value; 
+    productList[updatedIndex].price = productPrice.value;
     productList[updatedIndex].category = productCategory.value;
     productList[updatedIndex].description = productDescription.value;
-     myButton.innerHTML = "Add Product"
-    }
-    saveToLocalStorage();
-    displayProduct(productList);    
-    clearInputs();
-
-
-  
+    myButton.innerHTML = "Add Product";
   }
 
-  function displayProduct(pList) {
+  saveToLocalStorage();
+  displayProduct(productList);
+  clearInputs();
+}
+
+// Fungsi untuk menampilkan produk
+function displayProduct(pList) {
   var cartoona = "";
-  for (var i = 0; i < pList.length; i++)
-  {
-       cartoona += `
-      <tr >
+  for (var i = 0; i < pList.length; i++) {
+    // ❗️ Gunakan inline event handler (tidak direkomendasikan)
+    cartoona += `
+      <tr>
         <td> ${i+1} </td>
         <td>${pList[i].name}</td>
         <td>${pList[i].price}</td>
         <td>${pList[i].category}</td> 
         <td>${pList[i].description}</td>
-        <td class=""><button onclick="updateProduct(${i})" class="btn btn-outline-warning">Update</button></td>
-        <td class=""><button onclick="deleteProduct(${i})" class="btn btn-outline-danger">Delete</button></td>
+        <td><button onclick="updateProduct(${i})" class="btn btn-outline-warning">Update</button></td>
+        <td><button onclick="deleteProduct(${i})" class="btn btn-outline-danger">Delete</button></td>
       </tr>`;
-      
-  }
-  myBody.innerHTML = cartoona ;
-  }
- // function to clear inputs
-  function clearInputs() {
-    productName.value = "";
-     productPrice.value = "";
-     productCategory.value = "";
-      productDescription.value = "";
-
-
   }
 
+  myBody.innerHTML = cartoona;
+}
 
-  // function to delete product
+// Fungsi clear input
+function clearInputs() {
+  // ❗️ Unused return value (tidak masalah tapi bisa dianggap code smell)
+  return productName.value = productPrice.value = productCategory.value = productDescription.value = "";
+}
 
-  function deleteProduct(index){
-        productList.splice(index , 1);
-        saveToLocalStorage();
-       displayProduct(productList);
-  }
+// Fungsi hapus produk
+function deleteProduct(index) {
+  productList.splice(index, 1);
+  saveToLocalStorage();
+  displayProduct(productList);
+}
 
-
- 
- // function to save data in local storage
- function saveToLocalStorage() {
+// Simpan ke localStorage
+function saveToLocalStorage() {
+  // ❗️ Tidak ada error handling
   localStorage.setItem("productList", JSON.stringify(productList));
+}
 
-  
- }
- 
- function updateProduct(index) {
+// Fungsi update produk
+function updateProduct(index) {
   updatedIndex = index;
-  productName.value =  productList[index].name;
-     productPrice.value = productList[index].price;
-     productCategory.value = productList[index].category
-      productDescription.value =  productList[index].description
-      
-   myButton.innerHTML = "Update Product" 
-   }
+  productName.value = productList[index].name;
+  productPrice.value = productList[index].price;
+  productCategory.value = productList[index].category;
+  productDescription.value = productList[index].description;
+  myButton.innerHTML = "Update Product";
+}
 
+// Fungsi pencarian
+function searchProduct() {
+  var term = searchInput.value;
+  var searchList = [];
 
-
-
-
-
-   //function to search 
-   function searchProduct( ) {
-    var term = searchInput.value;
-     var searchList = [];
-   
-     for ( var i = 0; i < productList.length; i++ ) {
-      if(  
-        (productList[i].name.toLowerCase().includes(term.toLowerCase()) ) 
-      ||( productList[i].price.toLowerCase().includes(term.toLowerCase()))
-      || ( productList[i].category.toLowerCase().includes(term.toLowerCase()))
-      ||( productList[i].description.toLowerCase().includes(term.toLowerCase()))
-        
-        
-        )
-      
-      
-      
-      {
-        searchList.push(productList[i])
-
-      }
+  for (var i = 0; i < productList.length; i++) {
+    // ❗️ Tidak dicek null/undefined
+    if (
+      productList[i].name.toLowerCase().includes(term.toLowerCase()) ||
+      productList[i].price.toLowerCase().includes(term.toLowerCase()) || // ❗️ Angka dipaksa pakai toLowerCase
+      productList[i].category.toLowerCase().includes(term.toLowerCase()) ||
+      productList[i].description.toLowerCase().includes(term.toLowerCase())
+    ) {
+      searchList.push(productList[i]);
     }
-     displayProduct(searchList)
- 
-     }
+  }
+
+  displayProduct(searchList);
+}
